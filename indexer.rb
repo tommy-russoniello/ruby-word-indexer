951f4dd19b858164080fed5a8542c5831175aa01
Tommy Russoniello <tommy.russoniello@gmail.com>
2019-03-12 21:45:35 -0400

    Simplify and improve indexing logic and formatting

--- indexer.rb
+++ indexer.rb
@@ -1,26 +1,30 @@
-WORDS = {}
-FILES = []
+words = {}
+files = []
 
-ARGV.each { |file_path| FILES.push(File.open(file_path, "r")) }
+ARGV.each { |file_path| files.push(File.open(file_path, "r")) }
 
-file_count = 1
-FILES.each do |file|
+files.each.with_index do |file, file_index|
   word_count = 1
   file.each_line do |line|
-    words = line.split
-    words.each do |word|
-      word = word.downcase.tr('—–', '-').tr('’‘′', '\'').gsub(/[^a-z0-9'\-]/i, '') unless word =~ /\A[0-9]+\.[0-9]+\z/
+    line.split.each do |word|
+      word = word.downcase.tr('—–', '-').tr('’‘′', '\'').gsub(/[^a-z0-9'\-]/i, '') unless
+        word =~ /\A[0-9]+\.[0-9]+\z/
       next if word !~ /[a-z0-9]/i
-      WORDS[word] = ! WORDS[word]? { count: 1, indexes: ["#{file_count}-#{word_count}"] } :
-      { count: WORDS[word][:count] += 1,indexes: WORDS[word][:indexes] + ["#{file_count}-#{word_count}"] }
+
+      words[word] ||= { count: 0, indexes: [] }
+      words[word][:count] += 1
+      words[word][:indexes] += ["#{file_index + 1}-#{word_count}"]
       word_count += 1
     end
   end
-
-  file_count += 1
 end
 
-WORDS.sort { |a,b| -(a[1][:count] <=> b[1][:count]) }.each do |word, stats|
-  puts "'#{word}' is mentioned #{stats[:count]} time#{'s' unless stats[:count] == 1} at index\
-#{'es' unless stats[:count] == 1}\n  #{stats[:indexes].join(', ')}"
+words = words.sort do |a, b|
+  a.last[:count] == b.last[:count] ? a.first <=> b.first : -(a.last[:count] <=> b.last[:count])
+end
+
+words.each do |word, stats|
+  puts "'#{word}' is mentioned #{stats[:count]} time#{'s' unless stats[:count] == 1} at index" \
+    "#{'es' unless stats[:count] == 1}\n  #{stats[:indexes].join(', ')}"
+  puts
 end
